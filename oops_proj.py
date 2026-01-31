bf2074f16874c0e383be9295b9ac2db85f961ba2
Bhanu <dharavathubhanu340@gmail.com>
2026-01-31 11:55:31 -0500

    final commit of oops tutorial

--- oops_proj.py
+++ oops_proj.py
@@ -3,7 +3,7 @@ class chatbook:
         self.username=""
         self.password=""
         self.loggedin=False
-        self.menu()
+        # self.menu()
 
 
     def menu(self):
@@ -12,15 +12,19 @@ class chatbook:
                          2. Press 2 to signin
                          3. Press 3 to write a post
                          4. Press 4 to message a friend
-                         5. Press any other to exit""")
+                         5. Press any other to exit
+                         
+                         """)
         if user_input=="1":
             self.signup()
         elif user_input=="2":
             self.signin()
         elif user_input=="3":
-            pass
+            self.my_post()
         elif user_input=="4":
-            pass
+            self.sendmsg()
+
+        
         else:
             exit()
     def signup(self):
@@ -45,6 +49,28 @@ class chatbook:
                 print("Please input correct credentials..")
         print("\n")
         self.menu()
+    
+    def my_post(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here->")
+            print(f"Following content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt=input("Enter you message here ->")
+            frnd=input("Whom to send the msg? ->")
+            print("Your message has been sent to {frnd}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()    
+
+
+
 
 
 
--- rough.py
+++ rough.py
@@ -1,14 +1,21 @@
-lst=[1,2,3]
-my_str="mlops playlist"
-my_int=155
-# print(type(lst))
-# print(type(my_str))
-# lst.clear()
-# print(lst)
-# lst.capitilize()
-# print(lst)
+# lst=[1,2,3]
+# my_str="mlops playlist"
+# my_int=155
+# # print(type(lst))
+# # print(type(my_str))
+# # lst.clear()
+# # print(lst)
+# # lst.capitilize()
+# # print(lst)
 
-a='x'
-b='y'
-print(a+b)
+# a='x'
+# b='y'
+# print(a+b)
+
+from oops_proj import chatbook
+# user1=chatbook()
+lst=[1,2,3]
+print(len(lst))
+user1=chatbook()
+user1.sendmsg()
 
